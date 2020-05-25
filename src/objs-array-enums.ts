83efaa701ecdd5a2c394fba4f08eed2e0df70992
Richard Kotze <richard.kotze@thetrainline.com>
2020-05-25 17:42:32 +0100

    New section, new app js

---
+++ src/objs-array-enums.ts
@@ -0,0 +1,49 @@
+
+// Explicit object type
+const personA: { 
+  name: string;
+  age: number; 
+} = {
+  name: 'jones',
+  age: 29
+}
+
+// Prefer to infer types
+const person = {
+  name: 'jones',
+  age: 29,
+  hobbies: ['gaming', 'golf'],
+  role: [2, 'member']
+}
+
+let setHobbies: string[];
+setHobbies = ['ab'];
+
+console.log('Your code', person)
+
+// Tuple, need to be explicit on object type
+
+const personRole: { 
+  name: string;
+  age: number;
+  role: [number, string]; // tuple
+} = {
+  name: 'jones',
+  age: 29,
+  role: [1, 'author']
+}
+
+// personRole.role[0] = 'ksks'; will error as its a should be a number
+// personRole.role.push('abc'); no error, typescript exception
+
+
+// ENUMS
+
+enum Role { ADMIN, READ_ONLY, AUTHOR}
+
+const personEnum = {
+  name: 'jones',
+  age: 29,
+  role: Role.ADMIN
+}
+console.log("personEnum", personEnum)
